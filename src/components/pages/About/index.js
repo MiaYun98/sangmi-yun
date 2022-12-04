104d93329203b4d9a3b55e05c984c30acab40431
Mia <113086524+MiaYun98@users.noreply.github.com>
2022-12-04 13:46:06 -0800

    before depolouuy

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './style.css';
+
 import Profi from '../../../assets/mypicture.jpg'
 
 export default function About() {
@@ -6,9 +8,9 @@ export default function About() {
     <div className='about container p-4'>
       <h1 className='text-start'>About me</h1>
       <img className="profile row justify-content-start" src={Profi} alt="Sangmi Yun's Graduation picture"/>
-      <h1 className='text-end aboutname'>Sangmi Yun</h1>
+      {/* <h1 className='text-end aboutname'>Sangmi Yun</h1> */}
       <p className='text-start'>
-      Hello my name is sangmi Yun. I am born and raised in Korea for 20 years. I moved to the States about 4 years ago. I attended high school in Korea and was a captain of the English Newspaper club. I barely spoke English in Korea so when I first came here I had a hard time getting the confidence to speak with others. 
+      Hello my name is Sangmi Yun. I am born and raised in Korea for 20 years. I moved to the States about 4 years ago. I attended high school in Korea and was a captain of the English Newspaper club. I barely spoke English in Korea so when I first came here I had a hard time getting the confidence to speak with others. 
       </p>
       <p className='text-start'>
       I recently graduated from the University of Washington, with a Linguistic Major and an ASL minor. Currently, I am learning Full Stack web development from UW boot camp, and I'm going to earn certification in 2 weeks. 

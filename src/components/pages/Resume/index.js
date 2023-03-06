9201067b0cf952d669a30042e1d984b81c8a338e
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-03-06 10:51:32 -0800

    ASL

--- src/components/pages/Resume/index.js
+++ src/components/pages/Resume/index.js
@@ -43,7 +43,8 @@ export default function Resume() {
           <h3>Foreign Langauges</h3>
           <ul className="skills">
             <li>Korean - Native</li>
-            <li>Node.js</li>
+            <li>American Sign Langauge - Fluent</li>
+            <li>American Sign Langauge - Fluent</li>
             <li>Express.js</li>
             <li>MySQL</li>
             <li>MongoDB</li>
